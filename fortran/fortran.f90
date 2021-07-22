71b226ea4a0849996d3f6efc0727319c4fcd0ec6
Tunkert <tunkert@gmail.com>
2021-07-21 20:07:39 -0400

    complete fizzbuzz in python, c++, c, c#, java, and fortran

---
+++ fortran/fortran.f90
@@ -0,0 +1,23 @@
+program fizzbuzz
+implicit none
+
+	! define variables
+	integer :: n = 1
+
+	! do while loop
+	do while (n < 101)
+		IF (modulo(n, 3) == 0 .and. modulo(n, 5) == 0) THEN
+			print *, 'fizzbuzz'
+		ELSE IF (modulo(n, 3) == 0) THEN
+			print *, 'fizz' 
+		ELSE IF (modulo(n, 5) == 0) THEN 
+			print *, 'buzz'
+		ELSE	
+			print *, n
+		end if
+		n = n + 1
+	end do
+
+end program fizzbuzz
+
+
