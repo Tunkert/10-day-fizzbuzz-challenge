42677eca54ec5f3ce1c2da89c1fad0a978b4ba0f
Tunkert <tunkert@gmail.com>
2021-07-23 09:21:22 -0400

    solve fizzbuzz in swift

---
+++ swift/fizzbuzz.swift
@@ -0,0 +1,14 @@
+for n in 1...100 {
+  if n % 3 == 0 && n % 5 == 0 {
+    print("fizzbuzz")
+  }
+  else if n % 3 == 0 {
+    print("fizz")
+  }
+  else if n % 5 == 0 {
+    print("buzz")
+  }
+  else {
+    print("\(n)")
+  }
+}
